54296c49b4e758ba4a1cb99a94c548d77c29bf04
Yug Solanki <yugsolanki15@gmail.com>
2023-03-31 20:51:14 +0530

    login, logout, register done

--- base/urls.py
+++ base/urls.py
@@ -4,8 +4,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.loginUser, name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', views.logoutUser, name='logout'),
+    
     path('room/', views.room, name='room'),
     path('createroom/', views.createroom, name='createroom')   
 ]
